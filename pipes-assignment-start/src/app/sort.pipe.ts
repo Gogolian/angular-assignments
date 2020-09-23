97dc8cf903b3380bb5656d9d4e3e7bd85227c364
Gogolian <gogolian.snake@gmail.com>
2020-09-23 16:34:51 +0200

    pipes

---
+++ pipes-assignment-start/src/app/sort.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort'
+})
+export class SortPipe implements PipeTransform {
+
+  transform(value: [{name: string}]): [{name: string}] {
+    //return value
+     return value.sort((a,b)=>{
+       if(a.name.substr(0,1) > b.name.substr(0,1))
+       return -1
+       else return 1
+     })
+  }
+
+}
